6c8bc75816a1b76020a5f3d37c04d72e557dc9e6
Rishi Dwivedi <rishidwivedicode345@gmail.com>
2024-12-17 23:23:59 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -26,7 +26,7 @@ function decodeHTMLEntities(text) {
 // async function to get songs from songs folder
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`http://127.0.0.1:5501/${folder}/`);
+  let a = await fetch(`/${folder}/`);
   let response = await a.text();
   // console.log(response)
   let div = document.createElement("div");
@@ -91,7 +91,7 @@ const playMusic = (track, pause = false) => {
 
 // Function to display album
 async function displayAlbums() {
-  let a = await fetch(`http://127.0.0.1:5501/songs/`);
+  let a = await fetch(`/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
